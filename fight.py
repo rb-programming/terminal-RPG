c5c82ffd94275aebfadcf5d26ef6868363dc8cbc
rock bottom <322244589+rb-programming@users.noreply.github.com>
2026-09-17 20:00:00 +0300

    Implementation of attack speed and crit chance during combat

--- fight.py
+++ fight.py
@@ -1,6 +1,7 @@
 import enemy
 import random
 import time
+import math
 import ui
 
 from ui import reset, bold, green, yellow, magenta, red
@@ -29,6 +30,7 @@ def fight(player, backpack, difficulty):
     ui.enemy(enemy, difficulty)
     turn = "player"
     log = []
+    attack_remainder = 0
     while player.is_alive() and enemy.is_alive():
         ui.refresh(player)
         ui.enemy(enemy, difficulty)
@@ -36,15 +38,29 @@ def fight(player, backpack, difficulty):
             print(i)
         if turn == "player":
             time.sleep(0.5)
-            damage = calculate_damage(player, enemy)
-            enemy.get_damaged(damage)
-            if enemy.current_health > 0:
-                hit_message = f"{magenta + bold}{player.name}{reset} hits {magenta + bold}{enemy.name}{reset} for {red + bold}{int(damage)}{reset} damage."
-                log.append(hit_message)
-                print(hit_message)
-            else:
-                log.append(f"You have slain the {red + (enemy.name).lower() + reset}!")
-                print(f"You have slain the {red + (enemy.name).lower() + reset}!")
+            attack_remainder += player.attack_speed
+            if attack_remainder >= 1:
+                if math.isclose(attack_remainder, 2):
+                    attack_remainder = round(attack_remainder, 0)
+                for i in range(int(attack_remainder)):
+                    crit = False
+                    crit_chance = random.random()
+                    attack_remainder -= 1.0
+                    if player.crit < crit_chance:
+                        damage = calculate_damage(player, enemy)
+                    else:
+                        damage = calculate_damage(player, enemy) * 1.5
+                        crit = True
+                    enemy.get_damaged(damage)
+                    if enemy.current_health > 0:
+                        if crit:
+                            hit_message = f"{magenta + bold}{player.name}{reset} hits {magenta + bold}{enemy.name}{reset} with {yellow}all of his might{reset} for {red + bold}{int(damage)}{reset} damage."
+                        else:
+                            hit_message = f"{magenta + bold}{player.name}{reset} hits {magenta + bold}{enemy.name}{reset} for {red + bold}{int(damage)}{reset} damage."
+                        log.append(hit_message)
+                    else:
+                        log.append(f"You have slain the {red + (enemy.name).lower() + reset}!")
+                        break
             turn = 'enemy'
         else:
             time.sleep(0.5)
